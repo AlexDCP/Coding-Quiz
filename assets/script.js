117f23b49e2d7717fea8ae4b36438af5e4fc8ea2
AlexDCP <alexdcmarriot@gmail.com>
2023-06-12 14:13:41 -0700

    got working html, and timer. Started the rest

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,108 @@
-var introPage = document.querySelector("#intro");
-var startButton = document.querySelector("#start-button");
-var questionCard = document.querySelector("#question");
+var questions = document.getElementById("question");
+var choice1 = document.getElementById("choice1");
+var choice2 = document.getElementById("choice2");
+var choice3 = document.getElementById("choice3");
+var choice4 = document.getElementById("choice4");
 
-startButton.addEventListener("click", function(){
-    introPage.setAttribute('style', "display: none;")
-    questionCard.setAttribute('style', 'display: flex;')
-})
+var quiz = [
+  {
+    question: "1. When referring to a function, scope is __?",
+    choices: [
+      "Where the function is visible and accessible.",
+      "How far the function goes.",
+      "How long the function is.",
+      "The type of function it is (i.e. async...).",
+    ],
+    answer: "Where the function is visible and accessible.",
+  },
+  {
+    question: "2. When using javascript, if/else is used in __?",
+    choices: [
+      "A function.",
+      "A conditional statement.",
+      "A variable.",
+      "A loop.",
+    ],
+    answer: "A conditional statement.",
+  },
+  {
+    question: "3. When writing a function, return is used for __?",
+    choices: [
+      "Create a new variable.",
+      "To add a line break in the html.",
+      "Starting the next part of the function.",
+      "Closing out the function, and returning a value.",
+    ],
+    answer: "Closing out the function, and returning a value.",
+  },
+  {
+    question: "4. Why would you write a variable outside of a function?",
+    choices: [
+      "So it can be easily seen.",
+      "To keep your code clean.",
+      "To be able to update it quickly.",
+      "To allow all functions access to the variable, not just one.",
+    ],
+    answer: "To allow all functions access to the variable, not just one.",
+  },
+  {
+    question: "5. When using javascript for is used in a __?",
+    choices: ["Function.", "Variable.", "Loop.", "None of the above."],
+    answer: "Function.",
+  },
+];
 
+var questionIndex = 0;
+
+function displayQuestions() {
+  questions.textContent = quiz[questionIndex].question;
+  choice1.innerHTML = `
+    ${quiz[questionIndex].choices[0]} 
+    <input type="radio" name="radio" value="${quiz[questionIndex].choices[0]}" id="input1"/>
+    <span class="checkmark"></span>
+    `;
+  choice2.innerHTML = `
+    ${quiz[questionIndex].choices[1]} 
+    <input type="radio" name="radio" id="input2"/>
+    <span class="checkmark"></span>
+    `;
+  choice3.innerHTML = `
+    ${quiz[questionIndex].choices[2]} 
+    <input type="radio" name="radio" id="input3"/>
+    <span class="checkmark"></span>
+    `;
+  choice4.innerHTML = `
+    ${quiz[questionIndex].choices[3]} 
+    <input type="radio" name="radio" id="input4"/>
+    <span class="checkmark"></span>
+    `;
+}
+
+function checkAnswer() {
+  if (document.getElementById("input1").checked) {
+    if (
+      document.getElementById("input1").value === quiz[questionIndex].answer
+    ) {
+      questionIndex++;
+      displayQuestions();
+    } else {
+        questionIndex++;
+        displayQuestions();
+    }
+}
+//   } else if  (document.getElementById("input2").checked) {
+//     if (
+//       document.getElementById("input2").value === quiz[questionIndex].answer
+//     ) {
+//       questionIndex++;
+//       displayQuestions();
+//     } else {
+//         questionIndex++;
+//         displayQuestions();
+//     }
+//     }
+};
+
+document.getElementById("next").addEventListener("click", checkAnswer);
+
+displayQuestions();
